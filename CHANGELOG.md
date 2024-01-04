1f2dc484f90da0261f68a2cf9f6d1663090b1661
ecmel <ecmel.ercan@gmail.com>
2024-01-04 17:58:14 +0300

    added prefix

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 All notable changes to the extension will be documented in this file.
 
-## [1.0.1] - 2024-01-04
+## [1.0.2] - 2024-01-04
 
 - Update dependencies
 - Added changelog
--- src/provider.ts
+++ src/provider.ts
@@ -12,38 +12,41 @@ import { createWriteStream } from "fs";
 export async function archive() {
   const folder = await window.showWorkspaceFolderPick();
 
-  if (folder) {
-    const root = folder.uri.fsPath;
-    const name = `${folder.name}_${Date.now()}.zip`;
-    const dest = path.join(root, name);
-    const output = createWriteStream(dest);
-    const archive = archiver("zip");
-
-    const files = await walk({
-      path: root,
-      follow: false,
-      includeEmpty: false,
-      ignoreFiles: [".gitignore"],
-    });
-
-    output.on("error", (err) =>
-      window.showErrorMessage(`Failed to archive: ${err}`)
-    );
-
-    archive.on("error", (err) =>
-      window.showErrorMessage(`Failed to archive: ${err}`)
-    );
-
-    archive.on("end", () =>
-      window.showInformationMessage(`Archived ${files.length} files to ${name}`)
-    );
-
-    archive.pipe(output);
-
-    for (const file of files) {
-      archive.file(path.join(root, file), { name: file });
-    }
-
-    await archive.finalize();
+  if (!folder) {
+    return;
   }
+
+  const name = folder.name;
+  const root = folder.uri.fsPath;
+  const arch = `${name}_${Date.now()}.zip`;
+  const dest = path.join(root, arch);
+  const output = createWriteStream(dest);
+  const archive = archiver("zip");
+
+  const files = await walk({
+    path: root,
+    follow: false,
+    includeEmpty: false,
+    ignoreFiles: [".gitignore"],
+  });
+
+  output.on("error", (err) =>
+    window.showErrorMessage(`Failed to archive: ${err}`)
+  );
+
+  archive.on("error", (err) =>
+    window.showErrorMessage(`Failed to archive: ${err}`)
+  );
+
+  archive.on("end", () =>
+    window.showInformationMessage(`Archived ${files.length} files to ${arch}`)
+  );
+
+  archive.pipe(output);
+
+  for (const file of files) {
+    archive.file(path.join(root, file), { prefix: name, name: file });
+  }
+
+  await archive.finalize();
 }
